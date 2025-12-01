0f72b57f8c5cb6b8f20a2f194112fec0785c730f
plifip <philippefaisandier@gmail.com>
2025-12-01 23:52:33 +0100

    day 1

--- src/days/D1.ts
+++ src/days/D1.ts
@@ -1,7 +1,28 @@
 export const star1 = (data: string) => {
-    console.log('dataa: ', data);
+    let dial = 50;
+    let sum = 0;
+    for (const l of data.split('\n')) {
+        const rotation = l[0] === 'L' ? -1 : 1;
+        const n = +l.slice(1);
+        dial = (dial + n * rotation + 100) % 100;
+        if (dial === 0) sum++
+    }
+    console.log(`RESULT:: `, sum);
 }
 
 export const star2 = (data: string) => {
-
+    let dial = 50;
+    let sum = 0;
+    let direction = 'R';
+    for (const l of data.split('\n')) {
+        if (l[0] !== direction) {
+            dial = 100 - (dial || 100);
+            direction = l[0];
+        }
+        let n = +l.slice(1);
+        dial += n;
+        sum += Math.floor(dial / 100);
+        dial %= 100;
+    }
+    console.log(`RESULT:: `, sum);
 }
