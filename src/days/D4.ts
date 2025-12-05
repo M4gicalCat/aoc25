e86327d4e930360bcfa10b0d71951e1f04b9bdb6
plifip <philippefaisandier@gmail.com>
2025-12-05 22:28:51 +0100

    day 4

--- src/days/D4.ts
+++ src/days/D4.ts
@@ -1,8 +1,50 @@
 export const star1 = (data: string) => {
-
+    const grid = data.split('\n').map(line => line.split('').map(x => x === '@'))
+    let sum = 0;
+    for (let x = 0; x < grid.length; x++) {
+        for (let y = 0; y < grid[x].length; y++) {
+            if (!grid[x][y]) continue;
+            let n = 0;
+            if (grid[x-1]?.[y-1]) n++;
+            if (grid[x-1]?.[y]) n++;
+            if (grid[x-1]?.[y+1]) n++;
+            if (grid[x]?.[y-1]) n++;
+            if (grid[x]?.[y+1]) n++;
+            if (grid[x+1]?.[y-1]) n++;
+            if (grid[x+1]?.[y]) n++;
+            if (grid[x+1]?.[y+1]) n++;
+            if (n < 4) sum ++
+        }
+    }
+    console.log(sum);
 }
 
 export const star2 = (data: string) => {
-
+    const grid = data.split('\n').map(line => line.split('').map(x => x === '@'))
+    let sum = 0;
+    let innerSum = 0;
+    do {
+        innerSum = 0;
+        for (let x = 0; x < grid.length; x++) {
+            for (let y = 0; y < grid[x].length; y++) {
+                if (!grid[x][y]) continue;
+                let n = 0;
+                if (grid[x-1]?.[y-1]) n++;
+                if (grid[x-1]?.[y]) n++;
+                if (grid[x-1]?.[y+1]) n++;
+                if (grid[x]?.[y-1]) n++;
+                if (grid[x]?.[y+1]) n++;
+                if (grid[x+1]?.[y-1]) n++;
+                if (grid[x+1]?.[y]) n++;
+                if (grid[x+1]?.[y+1]) n++;
+                if (n < 4) {
+                    grid[x][y] = false;
+                    innerSum++
+                }
+            }
+        }
+        sum += innerSum;
+    } while (innerSum > 0)
+    console.log(sum);
 }
 
